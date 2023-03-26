ea9c863762f166eabcd869eabc859e198dcc81bf
Feshkineugen <118047190+Feshkineugen@users.noreply.github.com>
2023-03-26 21:12:57 +0300

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введиде название страны для поиска её столицы: ");
+            string s = Console.ReadLine();
+            // создание значения s которое будет вводить пользователь
+
+            // создаю словарь стран
+            Hashtable dict = new Hashtable()
+            {
+                ["Россия"] = "Москва",
+                ["Польша"] = "Варшава",
+                ["Япония"] = "Токио",
+                ["Китай"] = "Пекин",
+                ["Аргентина"] = "Буэнос-Айрес",
+                ["Австралия"] = "Канберра",
+                ["Австрия"] = "Вена",
+                ["США"] = "Вашингтон",
+                ["Турция"] = "Анкара",
+                ["Португалия"] = "Лиссабон"
+            };
+
+            // вывод данных на экран
+            Console.WriteLine($"Столица данной страны: {dict[s]}");
+        }
+    }
+}
